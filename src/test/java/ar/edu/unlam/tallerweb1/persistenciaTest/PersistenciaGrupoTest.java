012806fe6fa89d43cd0a0a8e64d4213ee3b261d3
zaito18 <k4tnet@outlook.com>
2021-05-23 01:23:22 -0300

    Creacion del modelo "grupo" mediante tdd.

---
+++ src/test/java/ar/edu/unlam/tallerweb1/persistenciaTest/PersistenciaGrupoTest.java
@@ -0,0 +1,40 @@
+package ar.edu.unlam.tallerweb1.persistenciaTest;
+
+import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.Grupo;
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import static org.assertj.core.api.Assertions.*;
+
+import javax.transaction.Transactional;
+
+public class PersistenciaGrupoTest extends SpringTest {
+
+
+    @Test @Transactional @Rollback
+    public void poderGuardarUnGrupo(){
+
+        Grupo grupoPicatecla = givenExisteUnGrupo();
+        Long idDelGrupo=whenGuardoElGrupo(grupoPicatecla);
+        thenLoPuedoBuscarPorId(idDelGrupo);
+    }
+
+    private Grupo givenExisteUnGrupo() {
+        return new Grupo();
+    }
+
+    private Long whenGuardoElGrupo(Grupo grupoPicatecla) {
+        session().save(grupoPicatecla);
+        return grupoPicatecla.getId();
+    }
+
+    private void thenLoPuedoBuscarPorId(Long idDelGrupo) {
+        Grupo buscado=session().get(Grupo.class, idDelGrupo);
+        assertThat(buscado).isNotNull();
+    }
+
+
+
+
+
+}
