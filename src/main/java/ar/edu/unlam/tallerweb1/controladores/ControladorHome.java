e51df1628bf52a1fb389d07031fb3743ded60ebf
marcelo1995tate <mzelaya@alumno.unlam.edu.ar>
2021-05-24 21:30:16 -0300

    ControladorHome

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorHome.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorHome.java
@@ -1,15 +1,36 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
 import ar.edu.unlam.tallerweb1.modelo.DatosDeGrupo;
+import ar.edu.unlam.tallerweb1.modelo.Grupo;
+import ar.edu.unlam.tallerweb1.servicios.ServicioGrupo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class ControladorHome {
 
-    @RequestMapping("/ir-a-crear-nuevo-grupo")
+    private final ServicioGrupo servicioGrupo;
+
+    @Autowired
+    public ControladorHome(ServicioGrupo servicioGrupo) {
+        this.servicioGrupo=servicioGrupo;
+    }
+
+    @RequestMapping("/home")
+    public ModelAndView irAHome() {
+        ModelMap model= new ModelMap();
+    List<Grupo> grupos=servicioGrupo.buscarTodos();
+    model.put("grupos",grupos);
+    return new ModelAndView("home",model);
+    }
+
+
+    @RequestMapping(value = "/ir-a-crear-nuevo-grupo")
     public ModelAndView irAlFormulario() {
         ModelMap model = new ModelMap();
         DatosDeGrupo datos= new DatosDeGrupo();
