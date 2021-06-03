95849defdf8e0907f94413069e698b57bf50014e
zaito18 <k4tnet@outlook.com>
2021-06-02 23:33:52 -0300

    view create group

---
+++ src/main/java/ar/edu/unlam/tallerweb1/util/exceptions/FormularioDeGrupoIncompleto.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.tallerweb1.util.exceptions;
+
+public class FormularioDeGrupoIncompleto extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+
+    public FormularioDeGrupoIncompleto() {
+        super("Completa todos los campos del formulario");
+    }
+    
+}
