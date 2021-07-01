8387488c6711034da4c1860393526075d661205a
marcelo1995tate <mzelaya@alumno.unlam.edu.ar>
2021-07-01 17:28:31 -0300

    service archivos

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServiciosArchivosImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServiciosArchivosImpl.java
@@ -1,11 +1,26 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
 import ar.edu.unlam.tallerweb1.modelo.Archivo;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioGrupo;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioMensaje;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.TreeSet;
 @Service
 public class ServiciosArchivosImpl implements ServicioArchivos{
+
+    private final RepositorioGrupo repoGrupo;
+    private final RepositorioUsuario repoUsuario;
+    private final RepositorioArchivos repositorioArchivos;
+
+    @Autowired
+    public ServicioMensajesImpl(RepositorioGrupo repoGrupo,RepositorioUsuario repoUsuario, RepositorioArchivos repositorioArchivos) {
+        this.repoGrupo = repoGrupo;
+        this.repoUsuario = repoUsuario;
+        this.repositorioArchivos = repositorioArchivos;
+    }
 
     @Override
     public TreeSet<Archivo> buscarArchivosPorGrupo(Long id) {
