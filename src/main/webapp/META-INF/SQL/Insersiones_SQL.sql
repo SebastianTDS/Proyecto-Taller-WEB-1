8596309983cd97f3172d3e56adea2b55628afda3
marcelo1995tate <mzelaya@alumno.unlam.edu.ar>
2021-07-20 21:45:26 -0300

    refactor vistas y inserts

--- src/main/webapp/META-INF/SQL/Insersiones_SQL.sql
+++ src/main/webapp/META-INF/SQL/Insersiones_SQL.sql
@@ -1,5 +1,4 @@
-USE
-db;
+USE db;
 
 INSERT INTO carrera(nombre)
 VALUES ('Desarrollo Web'),
@@ -15,17 +14,17 @@ VALUES ('Basica I'),
        ('Programacion web II'),
        ('Diseño de aplicaciones web');
 
-INSERT INTO usuario(email, nombre, password, rol)
-VALUES ('GLOBAL', 'GLOBAL', 'GLOBAL', 'GLOBAL');
+INSERT INTO usuario(email, cantidadDeCalificaciones, calificacion, nombre, password, rol)
+VALUES ('GLOBAL', 0, 0, 'GLOBAL', 'GLOBAL', 'GLOBAL');
 
-INSERT INTO usuario(email, nombre, password, rol)
-VALUES ('usuario3look.com', 'Marcelo ZELAYA', '1234', 'Admin');
+INSERT INTO usuario(email, cantidadDeCalificaciones, calificacion, nombre, password, rol)
+VALUES ('usuario3@outlook.com', 0, 0, 'Marcelo ZELAYA', '1234', 'Admin');
 
-INSERT INTO usuario(email, nombre, password, rol)
-VALUES ('usuario1@outlook.com', 'Gonzalo FERNANDEZ', '1234', 'Admin');
+INSERT INTO usuario(email, cantidadDeCalificaciones, calificacion, nombre, password, rol)
+VALUES ('usuario1@outlook.com', 0, 0, 'Gonzalo FERNANDEZ', '1234', 'Admin');
 
-INSERT INTO usuario(email, nombre, password, rol)
-VALUES ('usuario2@outlook.com', 'Sebastián TRILLO', '1234', 'Admin');
+INSERT INTO usuario(email, cantidadDeCalificaciones, calificacion, nombre, password, rol)
+VALUES ('usuario2@outlook.com', 0, 0, 'Sebastián TRILLO', '1234', 'Admin');
 
 INSERT INTO usuario(email, nombre, password, rol)
 VALUES ('usuario4@outlook.com', 'Angelo ORDOÑES', '1234', 'Admin');
@@ -34,4 +33,8 @@ INSERT INTO grupo(cantidadMax, descripcion, nombre, cerrado, turno, carrera_id, 
 VALUES (1, 'Foro de la materia', '', 1, 'NOCHE', 1, 1, 1, 4),
        (1, 'Foro de la materia', '', 0, 'TARDE', 2, 2, 1, 4),
        (1, 'Foro de la materia', '', 1, 'NOCHE', 1, 3, 1, 4),
-       (1, 'Foro de la materia', '', 0, 'TARDE', 2, 4, 1, 4);
+       (1, 'Foro de la materia', '', 0, 'TARDE', 2, 4, 1, 4),
+       (1, 'Foro de la materia', '', 0, 'TARDE', 2, 5, 1, 4),
+       (1, 'Foro de la materia', '', 0, 'TARDE', 2, 6, 1, 4),
+       (1, 'Foro de la materia', '', 0, 'TARDE', 2, 7, 1, 4),
+       (1, 'Foro de la materia', '', 0, 'TARDE', 2, 8, 1, 4);
